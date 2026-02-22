ac214be936d0d729d344384743fb947e476c86e5
はじめましてやまだです。 <96092291+RRRRRRR-777@users.noreply.github.com>
2026-02-22 13:49:14 +0900

    test(ci): CIトリガー確認用にJSDocコメントを追加

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Tailwindクラスをマージする。
+ *
+ * clsxで条件付きクラスを結合し、tailwind-mergeで競合するクラスを解決する。
+ *
+ * @param inputs - マージするクラス値
+ * @returns マージされたクラス文字列
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
